65f31127b683b7e88f1d002684189fca51fcbdcf
Eva Naranjo <evita.naranjo@gmail.com>
2022-05-20 10:39:22 +0000

    add endpoint get person, all person, planet, all planet

--- src/main.py
+++ src/main.py
@@ -41,9 +41,55 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/people', methods=['GET'])
-def get_all_people():
-    return jsonify(People().get_all_people()),200
+@app.route('/person', methods=['GET'])
+def get_all_person():
+    persons = Person.query.all()
+    list_all_person = list(map(lambda person: person.serialize(),persons))
+    return jsonify(list_all_person),200
+
+@app.route("/person/<int:person_id>", methods=["GET"])
+def get_person(person_id):
+    person=Person.query.get(person_id)
+
+    if person==None:
+        resp = "This person does not exist"
+    else:
+        resp =person.serialize()
+    
+    return jsonify(resp),200
+
+@app.route('/planet', methods=['GET'])
+def get_planets_all():
+    planets = Planet.query.all()
+    list_all_planet = list(map(lambda planet: planet.serialize(),planets))
+    return jsonify(list_all_planet),200
+
+@app.route("/planet/<int:planet_id>", methods=["GET"])
+def get_planet(planet_id):
+    planet=Planet.query.get(planet_id)
+
+    if planet==None:
+        resp = "This planet does not exist"
+    else:
+        resp =planet.serialize()
+    
+    return jsonify(resp),200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user=User.query.get(user_id)
+    if user==None:
+        resp = "User does not exist"
+    else:
+        resp = user.serialize()
+
+    return jsonify(resp),200
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
